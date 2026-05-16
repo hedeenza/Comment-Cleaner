b4f7c969820371163be74f80dcab08b898aec82e
Zach Hedeen <hedeenza@gmail.com>
2026-05-16 15:55:44 -0400

    moved enums, structs, and functions to lib.rs; formatted (cargo fmt) and linted (cargo clippy)

---
+++ src/lib.rs
@@ -0,0 +1,213 @@
+use clap::Parser;
+use regex::Regex;
+use std::fs::File;
+use std::io::{BufRead, BufReader, Write};
+
+#[derive(clap::ValueEnum, Clone, PartialEq)]
+pub enum Language {
+    Python,
+    R,
+    Rust,
+}
+
+#[derive(clap::ValueEnum, Clone, Debug, PartialEq)]
+pub enum CommentTypes {
+    Full,
+    Trailing,
+    Block,
+}
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+pub struct Args {
+    /// File to process
+    #[arg(short, long)]
+    pub file: String,
+
+    #[clap(value_enum)]
+    /// Name of the language the program is written in
+    #[arg(short, long)]
+    language: Language,
+
+    /// Comment type to remove
+    #[arg(short, long, num_args = 1.., value_delimiter = ' ')]
+    include: Vec<CommentTypes>,
+}
+
+pub fn check_language<R>(args: Args, input_reader: BufReader<R>, output_file: File)
+where
+    R: std::io::Read,
+{
+    if args.language == Language::Python {
+        let full_comment = Regex::new(r"^#* ").unwrap();
+        let trailing_comment = Regex::new(r"^[^#].*#").unwrap();
+        let block_comment = Regex::new(r"'''").unwrap();
+        check_included(
+            args,
+            full_comment,
+            trailing_comment,
+            block_comment,
+            input_reader,
+            output_file,
+        );
+    } else if args.language == Language::R {
+        let full_comment = Regex::new(r"^#* ").unwrap();
+        let trailing_comment = Regex::new(r"^[^#].*#").unwrap();
+        let block_comment = Regex::new(r"^#* ").unwrap();
+        check_included(
+            args,
+            full_comment,
+            trailing_comment,
+            block_comment,
+            input_reader,
+            output_file,
+        );
+    } else if args.language == Language::Rust {
+        let full_comment = Regex::new(r"^/* ").unwrap();
+        let trailing_comment = Regex::new(r"^[^//].*//").unwrap();
+        let block_comment = Regex::new(r"/\*|\*/").unwrap();
+        check_included(
+            args,
+            full_comment,
+            trailing_comment,
+            block_comment,
+            input_reader,
+            output_file,
+        );
+    }
+}
+
+pub fn check_included<R>(
+    args: Args,
+    full_comment: regex::Regex,
+    trailing_comment: regex::Regex,
+    block_comment: regex::Regex,
+    input_reader: BufReader<R>,
+    mut output_file: File,
+) where
+    R: std::io::Read,
+{
+    let mut keep_line = true;
+
+    if args.include.contains(&CommentTypes::Full)
+        && args.include.contains(&CommentTypes::Trailing)
+        && args.include.contains(&CommentTypes::Block)
+    {
+        for line in input_reader.lines() {
+            let line = line.unwrap();
+            if keep_line {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue;
+                } else if full_comment.is_match(&line) | trailing_comment.is_match(&line) {
+                    continue;
+                } else {
+                    let _ = writeln!(output_file, "{}", &line);
+                }
+            } else if !keep_line {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue;
+                }
+            } else {
+                println!("It is unclear whether to keep or skip the line");
+            }
+        }
+    } else if args.include.contains(&CommentTypes::Full)
+        && args.include.contains(&CommentTypes::Trailing)
+    {
+        for line in input_reader.lines() {
+            let line = line.unwrap();
+            if full_comment.is_match(&line) | trailing_comment.is_match(&line) {
+                continue;
+            } else {
+                let _ = writeln!(output_file, "{}", &line);
+            }
+        }
+    } else if args.include.contains(&CommentTypes::Full)
+        && args.include.contains(&CommentTypes::Block)
+    {
+        for line in input_reader.lines() {
+            let line = line.unwrap();
+            if keep_line {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue;
+                } else if full_comment.is_match(&line) {
+                    continue;
+                } else {
+                    let _ = writeln!(output_file, "{}", &line);
+                }
+            } else if !keep_line {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue;
+                }
+            } else {
+                println!("It is unclear whether to keep or skip the line");
+            }
+        }
+    } else if args.include.contains(&CommentTypes::Trailing)
+        && args.include.contains(&CommentTypes::Block)
+    {
+        for line in input_reader.lines() {
+            let line = line.unwrap();
+            if keep_line {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue;
+                } else if trailing_comment.is_match(&line) {
+                    continue;
+                } else {
+                    let _ = writeln!(output_file, "{}", &line);
+                }
+            } else if !keep_line {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue;
+                }
+            } else {
+                println!("It is unclear whether to keep or skip the line");
+            }
+        }
+    } else if args.include.contains(&CommentTypes::Full) {
+        for line in input_reader.lines() {
+            let line = line.unwrap();
+            if full_comment.is_match(&line) {
+                continue;
+            } else {
+                let _ = writeln!(output_file, "{}", &line);
+            }
+        }
+    } else if args.include.contains(&CommentTypes::Trailing) {
+        for line in input_reader.lines() {
+            let line = line.unwrap();
+            if trailing_comment.is_match(&line) {
+                continue;
+            } else {
+                let _ = writeln!(output_file, "{}", &line);
+            }
+        }
+    } else if args.include.contains(&CommentTypes::Block) {
+        for line in input_reader.lines() {
+            let line = line.unwrap();
+            if keep_line {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue;
+                } else {
+                    let _ = writeln!(output_file, "{}", &line);
+                }
+            } else if !keep_line {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue;
+                }
+            } else {
+                println!("It is unclear whether to keep or skip the line");
+            }
+        }
+    } else {
+        println!("No Comment Types Provided");
+    }
+}
