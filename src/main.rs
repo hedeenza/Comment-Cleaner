0017f3c1b3122117528038663c5e63a053f71117
Zach Hedeen <hedeenza@gmail.com>
2026-05-16 12:51:42 -0400

    refined comment-type selection/recognition functionality

--- src/main.rs
+++ src/main.rs
@@ -55,6 +55,13 @@ fn main() {
         "This has real code ###Then a comment later",
         "/* this one starts a block comment",
         "''' this one starts a block comment",
+        "In between a block comment 1",
+        "In between a block comment 2",
+        "In between a block comment 3",
+        "In between a block comment 4",
+        "In between a block comment 5",
+        "''' this one ends a block comment",
+        "*/ this one ends a block comment",
     ];
 
     check_language(args, lines);
@@ -79,8 +86,7 @@ fn check_language(args: Args, lines: Vec<&str>) {
         println!("rust");
         let full_comment = Regex::new(r"^/* ").unwrap();
         let trailing_comment = Regex::new(r"^[^//].*//").unwrap();
-        let block_comment = Regex::new(r"/\*").unwrap();
-        //let _block_end = Regex::new(r"\*/").unwrap();
+        let block_comment = Regex::new(r"/\*|\*/").unwrap();
         check_included(args, full_comment, trailing_comment, block_comment, lines);
     } else {
         ()
@@ -88,46 +94,122 @@ fn check_language(args: Args, lines: Vec<&str>) {
 }
 
 fn check_included(args: Args, full_comment: regex::Regex, trailing_comment: regex::Regex, block_comment: regex::Regex, lines: Vec<&str>) {
-    println!("INCLUDES: {:?}", args.include);
-    if args.include.contains(&CommentTypes::FullLine) && args.include.contains(&CommentTypes::TrailingLine) && args.include.contains(&CommentTypes::Block) {
-        println!("Full Line + Trailing Line + Block");
-        let filters = vec![full_comment.clone(), trailing_comment.clone(), block_comment.clone()];
-        filter_lines(filters, lines);
-    } else if args.include.contains(&CommentTypes::FullLine) && args.include.contains(&CommentTypes::TrailingLine) {
-        println!("Full Line + Trailing Line");
-        let filters = vec![full_comment.clone(), trailing_comment.clone()];
-        filter_lines(filters, lines);
-    } else if args.include.contains(&CommentTypes::FullLine) && args.include.contains(&CommentTypes::Block) {
-        println!("Full Line + Block");
-        let filters = vec![full_comment.clone(), block_comment.clone()];
-        filter_lines(filters, lines);
-    } else if args.include.contains(&CommentTypes::TrailingLine) && args.include.contains(&CommentTypes::Block) {
-        println!("Trailing Line + Block");
-        let filters = vec![trailing_comment.clone(), block_comment.clone()];
-        filter_lines(filters, lines);
-    } else if args.include.contains(&CommentTypes::FullLine) {
-        println!("FullLine");
-        let filters = vec![full_comment.clone()];
-        filter_lines(filters, lines);
-    } else if args.include.contains(&CommentTypes::TrailingLine) {
-        println!("TrailingLine");
-        let filters = vec![trailing_comment.clone()];
-        filter_lines(filters, lines);
-    } else if args.include.contains(&CommentTypes::Block) {
-        println!("Block");
-        let filters = vec![block_comment.clone()];
-        filter_lines(filters, lines);
-    } else {
-        println!("[ Error ]");
-    }
-}
+    let mut keep_line = true;
 
-fn filter_lines(filters: Vec<Regex>, lines: Vec<&str>) {
-    for line in &lines {
-        for filter in &filters {
-            if filter.is_match(&line) {
-                println!("{}: {}", filter, line);
+    if args.include.contains(&CommentTypes::FullLine) && args.include.contains(&CommentTypes::TrailingLine) && args.include.contains(&CommentTypes::Block) {
+        for line in lines {
+            if keep_line == true {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue
+                } else if full_comment.is_match(&line) {
+                    continue
+                } else if trailing_comment.is_match(&line) {
+                    continue
+                } else {
+                    println!("LINE: {}", line);
+                }
+            } else if keep_line == false {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue
+                }
+            } else {
+                println!("It is unclear whether to keep or skip the line");
             }
         }
+
+    } else if args.include.contains(&CommentTypes::FullLine) && args.include.contains(&CommentTypes::TrailingLine) {
+        for line in lines {
+            if full_comment.is_match(&line) {
+                continue
+            } else if trailing_comment.is_match(&line) {
+                continue
+            } else {
+                println!("LINE: {}", line);
+            }
+        }
+
+    } else if args.include.contains(&CommentTypes::FullLine) && args.include.contains(&CommentTypes::Block) {
+        for line in lines {
+            if keep_line == true {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue
+                } else if full_comment.is_match(&line) {
+                    continue
+                } else {
+                    println!("LINE: {}", line);
+                }
+            } else if keep_line == false {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue
+                }
+            } else {
+                println!("It is unclear whether to keep or skip the line");
+            }
+        }
+
+    } else if args.include.contains(&CommentTypes::TrailingLine) && args.include.contains(&CommentTypes::Block) {
+        for line in lines {
+            if keep_line == true {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue
+                } else if trailing_comment.is_match(&line) {
+                    continue
+                } else {
+                    println!("LINE: {}", line);
+                }
+            } else if keep_line == false {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue
+                }
+            } else {
+                println!("It is unclear whether to keep or skip the line");
+            }
+        }
+
+    } else if args.include.contains(&CommentTypes::FullLine) {
+        for line in lines {
+            if full_comment.is_match(&line) {
+                continue
+            } else {
+                println!("LINE: {}", line);
+            }
+        }
+
+    } else if args.include.contains(&CommentTypes::TrailingLine) {
+        for line in lines {
+            if trailing_comment.is_match(&line) {
+                continue
+            } else {
+                println!("LINE: {}", line);
+            }
+        }
+
+    } else if args.include.contains(&CommentTypes::Block) {
+        for line in lines {
+            if keep_line == true {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue
+                } else {
+                    println!("LINE: {}", line);
+                }
+            } else if keep_line == false {
+                if block_comment.is_match(&line) {
+                    keep_line = !keep_line;
+                    continue
+                }
+            } else {
+                println!("It is unclear whether to keep or skip the line");
+            }
+        }
+
+    } else {
+        println!("No Comment Types Provided");
     }
 }
